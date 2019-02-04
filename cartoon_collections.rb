3117016a2d345c32663e24531136288fc2afbb58
Carmen Renee <carmengenpt@gmail.com>
2019-02-04 23:37:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(planeteer_calls)
 end   
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.find
 end
